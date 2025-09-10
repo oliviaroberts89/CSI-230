db0bffd8638933f80713283991d4b1d33e726bca
Olivia <147193262+oliviaroberts89@users.noreply.github.com>
2025-09-09 23:57:40 -0400

    Add files via upload

---
+++ process_management_1.ps1
@@ -0,0 +1,17 @@
+﻿# Lists every process for which ProcessName starts with "C"
+Get-Process | Where-Object {$_.ProcessName -ilike "C*"}
+
+# Lists every process for which the path does not include the string "system32"
+Get-Process | Where-Object {$_.ProcessName -notlike "*system32*"}
+
+# Lists every stopped service, orders it alphabetically, and saves it to a csv file
+Get-Service | Where-Object {$_.Status -eq "Stopped"} | Sort-Object Name | 
+Export-Csv -Path "C:\Users\Olivia\Desktop\stopped_services.csv"
+
+# If not running already, starts Google Chrome web browser and directs it to Champlain.edu
+# If an instance is already running, stops it
+if (Get-Process -Name "chrome.exe") {
+    Stop-Process -Name "chrome.exe" -Force
+}
+else {
+    Start-Process "chrome.exe" "https://champlain.edu"}
---
+++ process_management_PART4ONLY.ps1
@@ -0,0 +1,7 @@
+﻿# If not running already, starts Google Chrome web browser and directs it to Champlain.edu
+# If an instance is already running, stops it
+if (Get-Process -Name "chrome.exe") {
+    Stop-Process -Name "chrome.exe" -Force
+}
+else {
+    Start-Process "chrome.exe" "https://champlain.edu"}
