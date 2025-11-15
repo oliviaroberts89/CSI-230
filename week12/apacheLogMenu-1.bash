35cceabf012769015f1b23ba0e078dd3046f325b
Olivia <147193262+oliviaroberts89@users.noreply.github.com>
2025-11-14 23:51:27 -0500

    Add files via upload

---
+++ week12/apacheLogMenu-1.bash
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+logFile="/var/log/apache2/access.log.1"
+
+function displayAllLogs(){
+	cat "$logFile"
+}
+
+function displayOnlyIPs(){
+        cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
+}
+
+# function: displayOnlyPages:
+# like displayOnlyIPs - but only pages
+
+function displayOnlyPages(){
+	cat "$logFile" | cut -d ' ' -f 7 | sort -n | uniq -c
+}
+
+function histogram(){
+
+	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '['  | sort \
+                              | uniq)
+	# This is for debugging, print here to see what it does to continue:
+	# echo "$visitsPerDay"
+
+        :> newtemp.txt  # what :> does is in slides
+	echo "$visitsPerDay" | while read -r line;
+	do
+		local withoutHours=$(echo "$line" | cut -d " " -f 2 \
+                                     | cut -d ":" -f 1)
+		local IP=$(echo "$line" | cut -d  " " -f 1)
+
+		local newLine="$IP $withoutHours"
+		echo "$IP $withoutHours" >> newtemp.txt
+	done
+	cat "newtemp.txt" | sort -n | uniq -c
+}
+
+# function: frequentVisitors:
+# Only display the IPs that have more than 10 visits
+# You can either call histogram and process the results,
+# Or make a whole new function. Do not forget to separate the
+# number and check with a condition whether it is greater than 10
+# the output should be almost identical to histogram
+# only with daily number of visits that are greater than 10
+
+function frequentVisitors(){
+
+	:> freq.txt
+
+	cat "$logFile" | cut -d " " -f 1,4 | tr -d "[" | while read -r ip datetime; do
+		date=$(echo "$datetime" | cut -d ":" -f1)
+		echo "$ip $date" >> freq.txt
+	done
+
+	sort freq.txt | uniq -c | while read -r count ip date; do
+		if ((count > 10)); then
+		    echo "# of visits, ip address, date"
+			echo "$count, $ip, $date"
+		fi
+	done
+}
+
+# function: suspiciousVisitors
+# Manually make a list of indicators of attack (ioc.txt)
+# filter the records with this indicators of attack
+# only display the unique count of IP addresses.
+# Hint: there are examples in slides
+
+# Keep in mind that I have selected long way of doing things to
+# demonstrate loops, functions, etc. If you can do things simpler,
+# it is welcomed.
+
+function suspiciousVisitors(){
+
+	declare -A ips
+
+	while read -r ioc; do
+		matches=$(grep "$ioc" "$logFile")
+
+		while read -r line; do
+			ip=$(echo "$line" | awk '{print $1}')
+			if [[ -n "$ip" ]]; then
+				((ips["$ip"]++))
+			fi
+		done <<< "$matches"
+	done < "ioc.txt"
+
+	for ip in "${!ips[@]}"; do
+		echo "${ips[$ip]} $ip"
+	done
+}
+
+
+while :
+do
+	echo "PLease select an option:"
+	echo "[1] Display all Logs"
+	echo "[2] Display only IPS"
+	# Display only pages visited
+	echo "[3] Display only pages visited"
+	echo "[4] Histogram"
+	# Frequent visitors
+	echo "[5] Display frequent visitors"
+	# Suspicious visitors
+	echo "[6] Display suspicious visitors"
+	echo "[7] Quit"
+
+	read userInput
+	echo ""
+
+	if [[ "$userInput" == "7" ]]; then
+		echo "Goodbye"
+		break
+
+	elif [[ "$userInput" == "1" ]]; then
+		echo "Displaying all logs:"
+		displayAllLogs
+
+	elif [[ "$userInput" == "2" ]]; then
+		echo "Displaying only IPS:"
+		displayOnlyIPs
+
+	# Display only pages visited
+   	elif [[ "$userInput" == "3" ]]; then
+        echo "Displaying only pages visited:"
+        displayOnlyPages
+
+	elif [[ "$userInput" == "4" ]]; then
+		echo "Histogram:"
+		histogram
+
+	elif [[ "$userInput" == "5" ]]; then
+	    echo "Displaying frequent visitors:"
+	    frequentVisitors
+
+	elif [[ "$userInput" == "6" ]]; then
+	    echo "Displaying suspicious visitors:"
+	    suspiciousVisitors
+
+        # Display frequent visitors
+	# Display suspicious visitors
+	# Display a message, if an invalid input is given
+	fi
+done
+
